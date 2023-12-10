d65057810d0e6d02a6f7c5d08720d494b10a29db
P-mm02 <poomtawee.mm02@gmail.com>
2023-12-11 05:39:45 +0700

    fix bug

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const port = process.env.PORT || 3333;
 // const fs = require('fs');
 const path = require('path');
+const itemsData = require('./itemsData.json');
+const { constants } = require('buffer');
+const { name } = require('ejs');
 
 // Setting the view engine and views directory for EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'views/img')));
 
@@ -17,6 +19,9 @@ app.get('/', (req, res) => {
   console.log('req SwipSwap')
   res.render('login')
 });
+app.get('/login', (req, res) => {
+  res.render('login')
+});
 app.get('/home', (req, res) => {
   // Process form data here
   // For example: Retrieve form data from req.body
@@ -24,11 +29,72 @@ app.get('/home', (req, res) => {
   // Render the home.ejs file as a response
   res.render('home');
 });
+app.get('/SwapList', (req, res) => {
+  res.render('SwapList')
+});
+app.get('/swapGive', (req, res) => {
+  res.render('swapGive')
+});
+app.get('/userInfo', (req, res) => {
+  res.render('userInfo')
+});
+app.get('/register', (req, res) => {
+  res.render('register')
+});
+app.get('/profileWindow', (req, res) => {
+  res.render('profileWindow')
+});
+app.get('/profile', (req, res) => {
+  const fileName = req.path.split('/').pop();
+  res.render('profile', { url: req.originalUrl })
+});
+app.get('/profile-swap', (req, res) => {
+  res.render('profile-swap')
+});
+app.get('/profile-history', (req, res) => {
+  res.render('profile-history')
+});
+app.get('/profile-giveaway', (req, res) => {
+  res.render('profile-giveaway')
+});
+app.get('/message', (req, res) => {
+  res.render('message')
+});
+app.get('/GiveList', (req, res) => {
+  res.render('GiveList')
+});
+app.get('/addSwap', (req, res) => {
+  res.render('addSwap')
+});
+app.get('/addGive', (req, res) => {
+  res.render('addGive')
+});
+app.get('/navigation', (req, res) => {
+  res.render('nav2'); // Renders the nav2.ejs template and sends HTML as a response
+});
+app.get('/profileWindow', (req, res) => {
+  res.render('profileWindow', { url: req.originalUrl }); // Renders the profileWindow.ejs template and sends HTML as a response
+});
+
+
+app.get('/s/:items', (req, res) => {
+  const username = 'XXX'; // Example username
+  const {items} = req.params;
+  const data = itemsData[items];
+
+  if(data){
+    res.render('tutorial', { username, ...data });
+  }else{
+    res.send('<h1>This Page Do Not Exit!!!</h1>');
+  }
+
+
+});
 
 // app.use((req, res) => {
 //   console.log("Got New Req")
-//   // res.send("weeeeee")
-//   res.send('<h1>xxxxxxxxxxweeeeee<h1>')
+//   // res.send("eeeeee")
+//   res.send('<h1>xxxxxxxxxxeeeeee<h1>')
 // })
 
 app.get('*', (req, res) => {
--- views/nav2.js
+++ views/nav2.js
@@ -1,10 +1,20 @@
-$(function(){
-    $("#navigation").load("nav2.ejs");    
-    $("body").css("margin-top", 200 + "px");
-});
-$(function(){
-    $("#profileWindow").load("profileWindow.ejs"); 
-});
+// $(function(){
+//     $("#navigation").load("nav2.ejs");    
+//     $("body").css("margin-top", 200 + "px");
+// });
+// $(function(){
+//     $("#profileWindow").load("profileWindow.ejs"); 
+// });
 
+$(function() {
+    $.get('/navigation', function(data) {
+        $('#navigation').html(data); // Load the navigation content into #navigation
+        $("body").css("margin-top", 200 + "px");
+    });
+
+    $.get('/profileWindow', function(data) {
+        $('#profileWindow').html(data); // Load the profile window content into #profileWindow
+    });
+});
 
  
