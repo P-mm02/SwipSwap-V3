d3d3559800c904cd38f19b760d6ef0551c1c6d01
P-mm02 <poomtawee.mm02@gmail.com>
2023-12-10 20:44:12 +0700

    add img

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3333;
+// const fs = require('fs');
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'views/img')));
+
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  console.log('req SwipSwap')
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+// app.use((req, res) => {
+//   console.log("Got New Req")
+//   // res.send("weeeeee")
+//   res.send('<h1>xxxxxxxxxxweeeeee<h1>')
+// })
+
+app.get('*', (req, res) => {
+  res.send('<h1>This Page Do Not Exit!!!</h1>');
 });
 
 // Start the server
